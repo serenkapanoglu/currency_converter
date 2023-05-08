d0b8b4c78b2c94e0f91182edbea4369ee0739e5f
serenkapanoglu <70541836+serenkapanoglu@users.noreply.github.com>
2023-05-08 17:01:53 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, jsonify,redirect
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    from_currency = request.form['from_currency']
+    to_currency = request.form['to_currency']
+    amount = request.form['amount']
+    
+    valid_currencies_response = requests.get('https://api.exchangerate.host/symbols')
+    valid_currencies = valid_currencies_response.json()['symbols']
+    if from_currency not in valid_currencies:
+        return render_template("error.html" ,message = f"Not a valid code:{from_currency}")
+    if  to_currency not in  valid_currencies:
+        return render_template("error.html", message=f"Not a valid code:{to_currency}")
+    
+    try:
+        float(amount)
+    except ValueError:
+        return render_template("error.html", message="Not a valid amount.")
+    
+    
+    response = requests.get(f'https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}')
+    data = response.json()
+    if to_currency=="EUR":
+        result = f"The result is €{data['result']}."
+    else:
+        result= f"The result is {data['result']} {to_currency}."
+    return render_template("result.html", result=result)
+        
+    
+if __name__ == '__main__':
+    app.run(debug=True)
+
+       
+
+   
+
+        
