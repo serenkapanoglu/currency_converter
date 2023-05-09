f2f3c4b3a658f6c8e40ae7b034a42ab50dc64b5d
serenkapanoglu <70541836+serenkapanoglu@users.noreply.github.com>
2023-05-08 23:39:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, jsonify,redirect
+from flask import Flask, render_template, request, jsonify,redirect,flash
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    
     return render_template('home.html')
 
 @app.route('/convert', methods=['POST'])
@@ -12,25 +13,33 @@ def convert():
     from_currency = request.form['from_currency']
     to_currency = request.form['to_currency']
     amount = request.form['amount']
-    
-    valid_currencies_response = requests.get('https://api.exchangerate.host/symbols')
-    valid_currencies = valid_currencies_response.json()['symbols']
-    if from_currency not in valid_currencies and to_currency not in  valid_currencies:
-        return render_template("home.html" ,message1 = f"Not a valid code:{from_currency}",message2 = f"Not a valid code:{to_currency}")
-    
+    message1 = ''
+    message2 = ''
+    message3 = ''
     
     try:
         float(amount)
     except ValueError:
-        return render_template("home.html", message3="Not a valid amount.")
+        message3 = "Not a valid amount."
+    valid_currencies_response = requests.get('https://api.exchangerate.host/symbols')
+    valid_currencies = valid_currencies_response.json()['symbols']
+    
+    if from_currency not in valid_currencies:
+        message1 = f"Not a valid code:{from_currency}"
+        
+    if to_currency not in valid_currencies:
+        message2 = f"Not a valid code:{to_currency}"
+        
+    if message1 or message2 or message3:
+        return render_template("home.html", message1=message1, message2=message2, message3=message3)
     
     
     response = requests.get(f'https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}')
     data = response.json()
     if to_currency=="EUR":
-        result = f"The result is €{data['result']}."
+        result = f"The result is €{str(round(data['result'],2))}."
     else:
-        result= f"The result is {data['result']} {to_currency}."
+        result= f"The result is {str(round(data['result'],2))} {to_currency}."
     return render_template("result.html", result=result)
         
     
